f3ee169d6531594aaf31ed853f95d7d7f68a0d9a
Raghu5968 <raghukarthik68@gmail.com>
2024-01-18 16:28:48 +0530

    change staticfiles

--- foodapp/settings.py
+++ foodapp/settings.py
@@ -133,6 +133,7 @@ MEDIA_URL='/images/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
 # Default primary key field type
